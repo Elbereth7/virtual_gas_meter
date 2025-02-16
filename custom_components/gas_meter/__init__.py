4ea2637b0b0d2554273b1ea62a3f55c9a855328b
Nataliya Yakovenko <natalia.yakov@gmail.com>
2025-02-16 11:46:07 +0100

    Update __init__.py

--- custom_components/gas_meter/__init__.py
+++ custom_components/gas_meter/__init__.py
@@ -4,7 +4,6 @@ from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.components.recorder.history import get_significant_states
 from homeassistant.util import dt as dt_util
 from homeassistant.helpers.discovery import async_load_platform
-from homeassistant.helpers.typing import ConfigType
 import custom_components.gas_meter.file_handler as fh
 DOMAIN = "gas_meter"
 _LOGGER = logging.getLogger(__name__)
@@ -12,11 +11,8 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the virtual gas meter integration."""
     _LOGGER.info("Loading Virtual Gas Meter integration...")
-    # Register a custom service to trigger actions
     async def handle_trigger_service(call: ServiceCall):
-        """Handle the custom service to run gas meter logic."""
-        _LOGGER.debug("Service call received: %s", call.data)
-        # Custom logic
+        """Handle the service call to update gas meter data."""
         try:
             gas_consume = await fh.load_gas_actualdata(hass)
             gas_new_datetime = fh.string_to_datetime(call.data["datetime"])
@@ -31,8 +27,6 @@ async def async_setup(hass: HomeAssistant, config: dict):
                 start_time = dt_util.as_utc(gas_prev_datetime)
                 end_time = dt_util.as_utc(gas_new_datetime)
                 entity_id = "switch.kociol_l1"
-
-                # Directly pass hass to get_significant_states, without needing to call get_instance
                 history_list = await hass.async_add_executor_job(
                     get_significant_states, hass, start_time, end_time, [entity_id]
                 )
@@ -42,8 +36,6 @@ async def async_setup(hass: HomeAssistant, config: dict):
                 previous_state = None
                 previous_time = start_time
                 for state in history_list.get(entity_id, []):
-                    _LOGGER.info("History list record last_changed: %s", state.last_changed)
-                    _LOGGER.info("History list record state: %s", state.state)
                     current_time = state.last_changed
 
                     if previous_state == "on":
@@ -80,21 +72,19 @@ async def async_setup(hass: HomeAssistant, config: dict):
                     gas_consume[-1]["average m3/min"] = av_min
 
                     hass.states.async_set(f"{DOMAIN}.average_m3_per_min", av_min)
-
+                    
             hass.states.async_set(f"{DOMAIN}.latest_gas_update", gas_new_datetime)
             hass.states.async_set(f"{DOMAIN}.latest_gas_data", gas_new_data)
 
-
-            # Save updated gas consumption latest_gas_update
+            # Save updated gas consumption
             await fh.save_gas_actualdata(gas_consume, hass)
 
         except Exception as e:
             _LOGGER.error("Error in handle_trigger_service: %s", str(e))
             raise
+            
     async def read_gas_actualdata_file(call: ServiceCall):
-        """Handle the custom service to run gas meter logic."""
-        _LOGGER.debug("Service call received: %s", call.data)
-        # Custom logic here
+        """Read and log gas meter data."""
         try:
             gas_consume = await fh.load_gas_actualdata(hass)
             for record in gas_consume:
