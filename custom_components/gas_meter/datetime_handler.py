73bf9aa97a432b873dc63c4625745d8c5eded911
Nataliya Yakovenko <natalia.yakov@gmail.com>
2025-02-16 11:46:46 +0100

    Update datetime_handler.py

--- custom_components/gas_meter/datetime_handler.py
+++ custom_components/gas_meter/datetime_handler.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta, date
 
-# 2025-01-12T14:50:00.000Z
 def string_to_datetime(datetime_string):
     try:
         gas_datetime = datetime.strptime(datetime_string, '%Y-%m-%d %H:%M:%S.%f%z')
