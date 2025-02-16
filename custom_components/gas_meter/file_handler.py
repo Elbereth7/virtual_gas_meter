7d5414ec9c6d8ce8e887650ecb30147857f0c622
Nataliya Yakovenko <natalia.yakov@gmail.com>
2025-02-16 11:50:53 +0100

    Update file_handler.py

--- custom_components/gas_meter/file_handler.py
+++ custom_components/gas_meter/file_handler.py
@@ -1,23 +1,24 @@
 from pathlib import Path
-from functools import wraps
-import csv
 from custom_components.gas_meter.datetime_handler import string_to_datetime
 import pickle
 from custom_components.gas_meter.gas_consume import GasConsume
 import aiofiles
 
-# Function to get the correct file path dynamically
 def get_gas_actualdata_path(hass):
+    """Returns the path to the gas consumption data file."""
     return Path(hass.config.path("custom_components/gas_meter/gas_actualdata.pkl"))
 
-# Function to save gas actual data in a file
 async def save_gas_actualdata(gas_consume, hass):
+    """Saves the gas consumption data to a file asynchronously."""
     filename = get_gas_actualdata_path(hass)
     async with aiofiles.open(filename, "wb") as file:
         await file.write(pickle.dumps(gas_consume))
 
-# Function to load gas actual data from a file
 async def load_gas_actualdata(hass):
+    """
+    Loads gas consumption data from a file asynchronously.
+    If the file does not exist, returns a new GasConsume object.
+    """
     filename = get_gas_actualdata_path(hass)
     try:
         async with aiofiles.open(filename, "rb") as file:
