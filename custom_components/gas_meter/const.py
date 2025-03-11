c7864beb3137c50a7a0167381b0c69dcc3c7a196
Nataliya Yakovenko <natalia.yakov@gmail.com>
2025-03-11 17:08:29 +0100

    Create const.py

---
+++ custom_components/gas_meter/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Virtual Gas Meter integration."""
+
+DOMAIN = "gas_meter"
+CONF_BOILER_ENTITY = "boiler_entity"
+CONF_BOILER_AVERAGE = "boiler_average"
+CONF_LATEST_GAS_DATA = "latest_gas_data"
+DEFAULT_BOILER_AV_H = 0.64153071524727
+DEFAULT_BOILER_AV_M = DEFAULT_BOILER_AV_H / 60
+DEFAULT_LATEST_GAS_DATA = 0
+DEFAULT_BOILER_ENTITY = "switch.kociol_l1"
